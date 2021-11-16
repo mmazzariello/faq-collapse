e36b53269646c5566c4227bd016baba2434e3a36
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-11-16 16:29:31 +0100

    DELETE console.log

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const toggles = document.querySelectorAll(".faq-toggle");
 
 toggles.forEach(function (el) {
   el.addEventListener("click", () => {
-    console.log("hola", el.parentNode);
     el.parentNode.classList.toggle("active");
   });
 });
