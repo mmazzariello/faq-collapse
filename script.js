08cafc5e3ac1db517a160c736efe1f58a7008be9
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-11-16 16:27:32 +0100

    UPDATE js with function keyword in forEach

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggles = document.querySelectorAll(".faq-toggle");
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
+
+toggles.forEach(function (el) {
+  el.addEventListener("click", () => {
+    console.log("hola", el.parentNode);
+    el.parentNode.classList.toggle("active");
   });
 });
